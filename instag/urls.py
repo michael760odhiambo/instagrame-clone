44c71a9b8d39b5621bcbd3691748dc8f56fdaef9
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-11 16:24:06 +0300

    added the media direction to the urls

--- instag/urls.py
+++ instag/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$', views.home, name='home'),
     url('^posts/', views.posts, name='posts'),
     url('^pastposts/', views.pastposts, name='pastposts'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

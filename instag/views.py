156172a3b08a5dab669ad8f6b41b56d1de4148cd
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-11 12:13:20 +0300

    added date time function

--- instag/views.py
+++ instag/views.py
@@ -8,8 +8,19 @@ def posts(request):
     date = dt.date.today()
     return render(request, 'all-pages/posts.html', {'date':date})    
 
-def pastposts(request):
-    date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+def pastposts(request, past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(posts)
+
     return render(request, 'all-pages/pastposts.html', {'date':date})    
 
 # Create your views here.

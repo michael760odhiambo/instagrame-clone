18aadb5b938959cfac7faaef958998e895234547
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-11 16:14:31 +0300

    added class comment author field

--- instag/models.py
+++ instag/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Comment(models.Model):
-#     author = models.CharField(max_length=60)
+     author = models.CharField(max_length=60)
 #     body = models.TextField()
 #     created_on = models.DateTimeField(auto_now_add=True)
     

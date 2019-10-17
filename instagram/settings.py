ea52e1d2176a0fccf8b62e9e2d2cd670e72f0661
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-17 14:52:16 +0300

    deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -149,7 +149,7 @@ MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_USER_TLS = config('EMAIL_USER_TLS')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
